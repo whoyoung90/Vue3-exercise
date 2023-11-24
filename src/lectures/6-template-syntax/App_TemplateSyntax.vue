823349ec4b7ca40ebd4bdaeda1dfcb28e8433dfb
kim-wooyoung <whoyoung90@naver.com>
2023-11-24 16:21:21 +0900

    study: 6-template-syntax

--- src/lectures/6-template-syntax/App_TemplateSyntax.vue
+++ src/lectures/6-template-syntax/App_TemplateSyntax.vue
@@ -5,14 +5,17 @@
     <p v-once>{{ message }}</p>
     <button v-on:click="message = message + '!'">Click!</button>
     <hr />
-    <h2>HTML</h2>
+
+    <h2>v-html</h2>
     <p>{{ rawHtml }}</p>
     <p v-html="rawHtml"></p>
     <hr />
-    <h2>속성 바인딩</h2>
+
+    <h2>속성 바인딩(v-bind)</h2>
     <div :title="dynamicTitle">마우스를 올려보세요</div>
     <input type="text" value="홍길동" :disabled="isInputDisabled" />
     <input v-bind="attrs" />
+
     <h2>JavaScript</h2>
     {{ message.split("").reverse().join("") }}<br />
     {{ isInputDisabled ? "예" : "아니오" }}
@@ -23,12 +26,15 @@
 import { ref } from "vue";
 
 export default {
+  // vue는 template 문법을 이용해서 컴포넌트 인스턴스에 바인딩 할 수 있다.
   setup() {
     const message = ref("안녕하세요!");
     const rawHtml = ref("<strong>안녕하세요</strong>");
 
     const dynamicTitle = ref("안녕하세요!@@@@@!!!");
     const isInputDisabled = ref(false);
+
+    // v-bind는 다중 속성을 한꺼번에 객체로 바인딩 할 수 있다.
     const attrs = ref({
       type: "text",
       value: "12345678",
