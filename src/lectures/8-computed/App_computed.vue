2815797a17e284e630968948a34f9961832d3001
kim-wooyoung <whoyoung90@naver.com>
2023-12-04 11:52:44 +0900

    study: 8-computed

--- src/lectures/8-computed/App_computed.vue
+++ src/lectures/8-computed/App_computed.vue
@@ -7,7 +7,9 @@
     <p>{{ hasLecture }}</p>
     <p>{{ existLecture() }}</p>
     <p>{{ existLecture() }}</p>
+
     <button v-on:click="counter++">Counter: {{ counter }}</button>
+
     <h2>이름</h2>
     <p>{{ fullName }}</p>
   </div>
@@ -22,12 +24,13 @@ export default {
       name: "짐코딩",
       lectures: ["HTML/CSS", "JavaScript", "Vue3"],
     });
-
+    // computed가 성능면에서 비용이 더 적게 든다 => computed는 계산된 결과 값을 캐시해놓기 때문!
+    // 캐시가 다시 계산되는 시점은 반응형 데이터가 변경될 때
     const hasLecture = computed(() => {
       console.log("computed");
       return teacher.lectures.length > 0 ? "있음 🙂" : "없음 🥲";
     });
-
+    // 반면 method는 실행될 때마다(렌더링 될때마다)..!
     const existLecture = () => {
       console.log("method");
       return teacher.lectures.length > 0 ? "있음 🙂" : "없음 🥲";
@@ -35,9 +38,9 @@ export default {
 
     const counter = ref(0);
 
+    // computed에 setter함수를 활용해서 쓰기 가능한 속성으로 만들 수 있다
     const firstName = ref("홍");
     const lastName = ref("길동");
-
     const fullName = computed({
       get() {
         return firstName.value + " " + lastName.value;
@@ -48,6 +51,7 @@ export default {
     });
     console.log("Console 출력: ", fullName.value);
     fullName.value = "짐 코딩";
+
     return {
       teacher,
       hasLecture,
